4b33f6517f58702c5e8b2fcd2e9846fb660229d6
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2025-04-10 06:57:52 -0300

    update to use helper

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,24 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
+	data := envelope{
 		"status":      "available",
 		"environment": app.config.env,
 		"version":     version,
 	}
 
-	encoded, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process yout request", http.StatusInternalServerError)
-		return
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(encoded)
 }
