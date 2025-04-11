9609f4e837a988e02bfa0cdbeb3822f8412e8a94
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2025-04-11 18:06:13 -0300

    implement tests for service

---
+++ services/tag_test.go
@@ -0,0 +1,58 @@
+package services_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/jeanpsv/realworld-golang/models"
+	"github.com/jeanpsv/realworld-golang/services"
+	"github.com/jeanpsv/realworld-golang/services/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSelectShouldReturnList(t *testing.T) {
+	expectedTags := []*models.Tag{
+		{
+			ID:        1,
+			Name:      "testing",
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		},
+	}
+
+	mockedTagRepository := mocks.NewTagRepository(t)
+	mockedTagRepository.On("Select").Return(expectedTags, nil)
+
+	tagService := services.NewTagService(mockedTagRepository)
+	actualTags, err := tagService.List()
+	assert.Nil(t, err)
+
+	for index, expected := range expectedTags {
+		actual := actualTags[index]
+		assert.EqualValues(t, expected, actual)
+	}
+}
+
+func TestSelectShouldReturnEmptyList(t *testing.T) {
+	mockedTagRepository := mocks.NewTagRepository(t)
+	mockedTagRepository.On("Select").Return([]*models.Tag{}, nil)
+
+	tagService := services.NewTagService(mockedTagRepository)
+	actualTags, err := tagService.List()
+	assert.Nil(t, err)
+	assert.Empty(t, actualTags)
+}
+
+func TestSelectShouldReturnError(t *testing.T) {
+	expectedErrorMessage := "some error"
+	mockedTagRepository := mocks.NewTagRepository(t)
+	mockedTagRepository.On("Select").Return(nil, errors.New(expectedErrorMessage))
+
+	tagService := services.NewTagService(mockedTagRepository)
+	actualTags, err := tagService.List()
+
+	assert.Nil(t, actualTags)
+	assert.NotNil(t, err)
+	assert.Equal(t, expectedErrorMessage, err.Error())
+}
